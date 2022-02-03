bcfec3e1f5ac4cee93a1b7bfdeae7d29b8896a3c
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-02-03 11:03:09 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/************************************************************************************************** */
 window.addEventListener("load", () => {
     document.querySelector(".main").classList.remove("hidden");
     document.querySelector(".home").classList.add("active");
@@ -244,50 +245,38 @@ $(document).ready(function () {
     });
 });
 
-// this.width = this.$refs.card.offsetWidth;
-// this.height = this.$refs.card.offsetHeight;
+/* card */
+const wrapper = document.querySelectorAll(".cardWrap");
 
-// data: () => ({
-//     width: 0,
-//     height: 0,
-//     mouseX: 0,
-//     mouseY: 0,
-//     mouseLeaveDelay: null
-// });
+wrapper.forEach(element => {
+    let state = {
+        mouseX: 0,
+        mouseY: 0,
+        height: element.clientHeight,
+        width: element.clientWidth
+    };
 
-// function mousePX(){
-//     return this.mouseX / this.width;
-// }
+    element.addEventListener("mousemove", ele => {
+        const card = element.querySelector(".card");
+        const cardBg = card.querySelector(".cardBg");
+        state.mouseX = ele.pageX - element.offsetLeft - state.width / 2;
+        state.mouseY = ele.pageY - element.offsetTop - state.height / 2;
 
-// function mousePY(){
-//     return this.mouseY / this.height;
-// }
+        // parallax angle in card
+        const angleX = (state.mouseX / state.width) * 30;
+        const angleY = (state.mouseY / state.height) * -30;
+        card.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg) `;
 
-// function cardStyle(){
-//     const rX = this.mousePX * 30;
-//       const rY = this.mousePY * -30;
-//       return {
-//         transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
-//       };
-// }
+        // parallax position of background in card
+        const posX = (state.mouseX / state.width) * -40;
+        const posY = (state.mouseY / state.height) * -40;
+        cardBg.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
+    });
 
-// function cardBgTransform(){
-//     const tX = this.mousePX * -40;
-//       const tY = this.mousePY * -40;
-//       return {
-//         transform: `translateX(${tX}px) translateY(${tY}px)`
-//       };
-// }
-
-// function cardBgImage(){
-//     return {
-//         backgroundImage: `url(${this.dataImage})`
-//       };
-// }
-
-// function handleMouseMove(e) {
-//     this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
-//     this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
-// }
-
-
+    element.addEventListener("mouseout", () => {
+        const card = element.querySelector(".card");
+        const cardBg = card.querySelector(".cardBg");
+        card.style.transform = `rotateY(0deg) rotateX(0deg) `;
+        cardBg.style.transform = `translateX(0px) translateY(0px)`;
+    });
+});
